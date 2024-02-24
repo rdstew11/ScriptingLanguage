61afa41a866f5bf4f75a9603a5b43d87d325a87b
Ryan Stewart <rdstew11@gmail.com>
2024-02-24 11:44:54 -0500

    add function closures

--- src/com/rdstew/lox/LoxFunction.java
+++ src/com/rdstew/lox/LoxFunction.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function decalaration) {
+    LoxFunction(Stmt.Function decalaration, Environment closure) {
         this.declaration = decalaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         // Create function scope and execute body
-        Environment env = new Environment(interpreter.globals);
+        Environment env = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             env.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
