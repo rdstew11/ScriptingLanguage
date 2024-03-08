27604d3443a1444c5c2fa83f24caa7f324acad62
Ryan Stewart <rdstew11@gmail.com>
2024-03-07 20:34:46 -0500

    (CLOX) added value stack

--- src/clox/main.c
+++ src/clox/main.c
@@ -3,8 +3,11 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *arg[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
@@ -12,7 +15,8 @@ int main(int argc, const char *arg[]) {
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
