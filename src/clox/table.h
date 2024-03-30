04b8a6159f060e01bb2f08750400e3592d75a54b
Ryan Stewart <rdstew11@gmail.com>
2024-03-29 22:56:12 -0400

    Clox, start table impl

---
+++ src/clox/table.h
@@ -0,0 +1,22 @@
+#ifndef clox_table_h
+#define clox_tabl_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+
+#endif
