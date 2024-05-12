e9d1d5ba7da2d1bfe86910af3e90b1e473232ba7
Ryan Stewart <rdstew11@gmail.com>
2024-05-12 19:14:19 -0400

    Add compiler enclosures

--- src/compiler.c
+++ src/compiler.c
@@ -51,7 +51,9 @@ typedef enum {
     TYPE_SCRIPT
 } FunctionType;
 
-typedef struct{
+typedef struct Compiler{
+    struct Compiler* enclosing;
+
     ObjFunction* function;
     FunctionType type;
 
@@ -253,6 +255,7 @@ static void patchJump(int offset){
 }
 
 static void initCompiler(Compiler* compiler, FunctionType type){
+    compiler->enclosing = current;
     compiler->function = NULL;
     compiler->type = type;
     compiler->localCount = 0;
@@ -276,6 +279,7 @@ static ObjFunction* endCompiler() {
         printf("Parser had error\n");
     }
 #endif
+    current = current-> enclosing;
     return function;
 }
 
@@ -360,6 +364,7 @@ static uint8_t parseVariable(const char* errorMessage){
 }
 
 static void markInitialized() {
+    if (current->scopeDepth == 0) return;
     current->locals[current->localCount -1].depth = current->scopeDepth;
 }
 
@@ -553,6 +558,27 @@ static void block() {
     consume(TOKEN_RIGHT_BRACE, "Expected '}' after block.");
 }
 
+static void function(FunctionType type){
+    Compiler compiler;
+    initCompiler(&compiler, type);
+    beginScope();
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+    block();
+
+    ObjFunction* function = endCompiler();
+    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funcDefinition(){
+    uint8_t global = parseVariable("Expect function name.");
+    markInitialized();
+    function(TYPE_FUNCTION);
+    defineVariable(global);
+}
+
 static void expressionStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
@@ -687,7 +713,9 @@ static void synchronize(){
 }
 
 static void declaration() {
-    if (match(TOKEN_VAR)){
+    if (match(TOKEN_FUN)){
+        funcDefinition();
+    } else if (match(TOKEN_VAR)){
         varDeclaration();
     } else {
         statement();
